1c5322c6e7eb12a0d34798ce9a99770e8ceabd98
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-07-12 00:52:46 +0900

    Separate the setup of MySQL, PostgreSQL, and SQLite3 into individual scripts

---
+++ scripts/setup-db-mysql.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -eu
+
+sudo apt-get install -y --no-install-recommends default-mysql-client
+
+docker run \
+  --name redmine-mysql \
+  -e MYSQL_ROOT_PASSWORD=password \
+  -e MYSQL_DATABASE=redmine_test \
+  -p 3306:3306 \
+  -d $DATABASE \
+  --character-set-server=utf8mb4 \
+  --collation-server=utf8mb4_unicode_ci
+
+cd $REDMINE_DIR
+cat <<EOS > config/database.yml
+  test:
+    adapter: mysql2
+    database: redmine_test
+    host: 127.0.0.1
+    port: 3306
+    username: root
+    password: password
+    encoding: utf8mb4
+EOS
---
+++ scripts/setup-db-postgresql.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+sudo apt-get install -y --no-install-recommends libpq-dev
+
+docker run \
+  --name redmine-postgres \
+  -e POSTGRES_PASSWORD=postgres \
+  -p 5432:5432 \
+  -d $DATABASE
+
+cd $REDMINE_DIR
+cat <<EOS > config/database.yml
+  test:
+    adapter: postgresql
+    database: redmine_test
+    encoding: utf8
+    host: localhost
+    port: 5432
+    username: postgres
+    password: postgres
+EOS
