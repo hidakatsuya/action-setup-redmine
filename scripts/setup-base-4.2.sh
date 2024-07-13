927d7d64504eac12bf548dea6749be6f70bd5a89
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-07-14 01:25:27 +0900

    Run system tests when the version is also v4.2

--- scripts/setup-base-4.2.sh
+++ scripts/setup-base-4.2.sh
@@ -15,6 +15,10 @@ sudo apt-get install -y --no-install-recommends \
 # Allow ImageMagick to read PDF files
 sudo sed -ri 's/(rights)="none" (pattern="PDF")/\1="read" \2/' /etc/ImageMagick-6/policy.xml
 
+# Fix LoadError cannot load such file -- builder
+# https://www.redmine.org/issues/40802
+echo "gem 'builder', '~> 3.2.4'" >> $REDMINE_DIR/Gemfile.local
+
 # Set up Google Chrome for system tests
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google-chrome.list
@@ -22,6 +26,11 @@ sudo apt-get update
 sudo apt-get install -y --no-install-recommends google-chrome-stable
 echo GOOGLE_CHROME_OPTS_ARGS="headless,disable-gpu,no-sandbox,disable-dev-shm-usage" >> $GITHUB_ENV
 
-# Fix LoadError cannot load such file -- builder
-# https://www.redmine.org/issues/40802
-echo "gem 'builder', '~> 3.2.4'" >> $REDMINE_DIR/Gemfile.local
+sed -i -e '/gem "selenium-webdriver"/d' $REDMINE_DIR/Gemfile
+sed -i -e "/gem 'webdrivers'/d" $REDMINE_DIR/Gemfile
+echo "gem 'selenium-webdriver', '~> 4.8.6'" >> $REDMINE_DIR/Gemfile
+echo "gem 'webdrivers', '~> 5.2', require: false" >> $REDMINE_DIR/Gemfile
+sed -i -e 's/desired_capabilities/capabilities/' $REDMINE_DIR/test/application_system_test_case.rb
+
+cat $REDMINE_DIR/test/application_system_test_case.rb
+cat $REDMINE_DIR/Gemfile
