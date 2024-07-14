b34f777aac75d1ce9bd47139e934d55e359683cf
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-07-14 13:12:17 +0900

    Separate base environment process setup into setup script

---
+++ scripts/setup-base.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -eu
+
+# v5.1.3.stable -> 5.1.stable
+major_version_with_branch=$REDMINE_VERSION_MAJOR.$REDMINE_VERSION_MINOR.$REDMINE_VERSION_BRANCH
+
+case $major_version_with_branch in
+    5.1.devel)
+        setup_version="next";;
+    5.1*|5.0*|4.2*)
+        setup_version="$REDMINE_VERSION_MAJOR.$REDMINE_VERSION_MINOR";;
+    *)
+        setup_version="next";;
+esac
+
+echo "Running scripts/setup-base-$setup_version.sh for Redmine $REDMINE_VERSION ..."
+
+./scripts/setup-base-$setup_version.sh
