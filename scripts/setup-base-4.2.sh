0895d5e5668cf1f97869a8dd2cb55f90f7d13e06
Katsuya Hidaka <hidakatsuya@gmail.com>
2024-07-14 15:22:14 +0900

    Unify indent size of shell scripts

--- scripts/setup-base-4.2.sh
+++ scripts/setup-base-4.2.sh
@@ -5,12 +5,12 @@ set -eu
 # Install dependencies
 sudo apt-get update; \
 sudo apt-get install -y --no-install-recommends \
-  build-essential \
-  bzr git mercurial subversion cvs \
-  ghostscript \
-  gsfonts \
-  imagemagick libmagick++-dev \
-  libnss3-dev;
+     build-essential \
+     bzr git mercurial subversion cvs \
+     ghostscript \
+     gsfonts \
+     imagemagick libmagick++-dev \
+     libnss3-dev;
 
 # Allow ImageMagick to read PDF files
 sudo sed -ri 's/(rights)="none" (pattern="PDF")/\1="read" \2/' /etc/ImageMagick-6/policy.xml
--- scripts/setup-db-mysql.sh
+++ scripts/setup-db-mysql.sh
@@ -5,13 +5,13 @@ set -eu
 sudo apt-get install -y --no-install-recommends default-mysql-client
 
 docker run \
-  --name redmine-mysql \
-  -e MYSQL_ROOT_PASSWORD=password \
-  -e MYSQL_DATABASE=redmine_test \
-  -p 3306:3306 \
-  -d $DATABASE \
-  --character-set-server=utf8mb4 \
-  --collation-server=utf8mb4_unicode_ci
+    --name redmine-mysql \
+    -e MYSQL_ROOT_PASSWORD=password \
+    -e MYSQL_DATABASE=redmine_test \
+    -p 3306:3306 \
+    -d $DATABASE \
+    --character-set-server=utf8mb4 \
+    --collation-server=utf8mb4_unicode_ci
 
 cd $REDMINE_DIR
 cat <<EOS > config/database.yml
